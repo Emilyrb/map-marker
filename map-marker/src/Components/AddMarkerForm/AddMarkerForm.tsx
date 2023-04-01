25394c2efd3c431b04b91c77a807fa33fd502d37
emily-guan <emily.guan27@hotmail.com>
2023-04-02 02:19:55 +1000

    Update add marker form

--- map-marker/src/Components/AddMarkerForm/AddMarkerForm.tsx
+++ map-marker/src/Components/AddMarkerForm/AddMarkerForm.tsx
@@ -2,7 +2,7 @@ import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import styled from 'styled-components';
-import { StarRating } from './StarRating';
+import { useState } from 'react';
 
 const StyledContainer = styled(Container)`
   padding: 0;
@@ -19,7 +19,7 @@ const StyledForm = styled(Form)`
   width: 100%;
   border-radius: 10px; 
   background-color: rgba(255,255,255,0.8);
-  padding: 0;
+  padding: 10px;
   color: black;
 `;
 
@@ -29,11 +29,6 @@ const StyledRow = styled(Row)`
   align-items: center;
   justify-content: center;
   padding: 5px 0;
-`;
-
-const StyledCheckbox = styled(Form.Check)`
-  transform: scale(1.5);
-  height: 30px;
 `;
 
 const CloseFormButton = styled.div`
@@ -48,16 +43,33 @@ const CloseFormButton = styled.div`
 
 interface Props {
     setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
+    markerPos: {lat: number, lng: number};
 }
 
 export function AddMarkerForm(props: Props) {
-    const { setShowForm } = props;
+    const { setShowForm, markerPos } = props;
+    const [ formData, setFormData ] = useState({
+      'form.Location': '',
+      'form.Address': '',
+      'form.Image': '',
+  })
+
+  function handleChange(e: any) {
+    const key = e.target.id;
+    const value = e.target.value;
+    setFormData({...formData, [key]: value})
+  }
+
+  function handleSubmit(e: any) {
+    console.log('submit marker at', markerPos);
+    e.preventDefault();
+}
 
     return (
       <StyledContainer>
-        <StyledForm>
+        <StyledForm onSubmit={handleSubmit} id='addMarkerForm' >
           <StyledRow>
-            <Col><h1>Add Skate Park Marker</h1></Col>
+            <Col><h1>Add Marker</h1></Col>
             <Col xs={1}>
               <CloseFormButton onClick={() => {setShowForm(false)}}>
                 <FontAwesomeIcon icon={faCircleXmark} size='2xl' />
@@ -65,86 +77,24 @@ export function AddMarkerForm(props: Props) {
             </Col>
           </StyledRow>
           <StyledRow>
-            <Form.Group as={Col} controlId='form.Name'>
-              <Form.Label>Name</Form.Label>
-              <Form.Control type='text' placeholder='Emily' />
-            </Form.Group>
-          </StyledRow>
-          <StyledRow>
-            <Form.Group as={Col} controlId='form.Date'>
-              <Form.Label>Date</Form.Label>
-              <Form.Control type='date' />
-            </Form.Group>
-            <Form.Group as={Col} controlId='form.Time'>
-              <Form.Label>Time</Form.Label>
-              <Form.Control type='time' />
-            </Form.Group>
-          </StyledRow>
-          <StyledRow>
             <Form.Group as={Col} controlId='form.Location'>
                 <Form.Label>Location</Form.Label>
-                <Form.Control type='text' placeholder='Kuraby' />
+                <Form.Control type='text' placeholder='Kuraby' onChange={handleChange} />
             </Form.Group>
             <Form.Group as={Col} controlId='form.Address'>
                 <Form.Label>Address</Form.Label>
-                <Form.Control type='text' placeholder='1300A Beenleigh Rd, Kuraby QLD 4112' />
+                <Form.Control type='text' placeholder='1300A Beenleigh Rd, Kuraby QLD 4112' onChange={handleChange}/>
             </Form.Group>
           </StyledRow>
           <StyledRow>
-            <Form.Group as={Col} controlId='form.BeginnerFriendly'>
-              <Form.Label>Beginner Friendly</Form.Label>
-              <StarRating value={0} />
-            </Form.Group>
-            <Form.Group as={Col} controlId='form.AdvancedFriendly'>
-              <Form.Label>Advanced Friendly</Form.Label>
-              <StarRating value={0} />
-            </Form.Group>
+          <Form.Group controlId="form.Image">
+            <Form.Label>Upload Image</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
           </StyledRow>
-          <StyledRow>
-            <Form.Group as={Col} controlId='form.Safety'>
-              <Form.Label>Safety</Form.Label>
-              <StarRating value={0} />
-            </Form.Group>
-            <Form.Group as={Col}controlId='form.Busy'>
-              <Form.Label>Busy</Form.Label>
-              <StarRating value={0} />
-            </Form.Group>
-          </StyledRow>
-          <StyledRow>
-            <Form.Group as={Col} controlId='form.Ramps'>
-              <Form.Label>Ramps</Form.Label>
-              <Form.Control type='text' placeholder='2' />
-            </Form.Group>
-            <Form.Group as={Col} controlId='form.DropIns'>
-              <Form.Label>DropIns</Form.Label>
-              <Form.Control type='text' placeholder='3' />
-            </Form.Group>
-          </StyledRow>
-          <StyledRow>
-            <Form.Group as={Col} controlId='form.PumpTrack'>
-              <Form.Label>Pump Track</Form.Label>
-              <div>
-                <StyledCheckbox type="checkbox" id="form.PumpTrack" />
-              </div>
-            </Form.Group>
-            <Form.Group as={Col} controlId='form.Bowl'>
-              <Form.Label>Bowl</Form.Label>
-              <div>
-                <StyledCheckbox type="checkbox" id="form.Bowl" />
-              </div>
-            </Form.Group>
-          </StyledRow>
-          <StyledRow>
-            <Form.Group as={Col} controlId='form.OverallRating'>
-              <Form.Label>Overall Rating</Form.Label>
-              <StarRating value={0} />
-            </Form.Group>
-          </StyledRow>
-          <StyledRow>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </StyledRow>
+          <Button variant="primary" type="submit" form="addMarkerForm">
+            Submit
+          </Button>
         </StyledForm>
       </StyledContainer>
     );
